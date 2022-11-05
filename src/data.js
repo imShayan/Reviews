a02feb7d14d366b4b3728383033dba85f979bebe
Shayan Ahmed <im.shayan28@gmail.com>
2022-11-06 01:13:46 +0530

    uploading reviews

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Susan Smith',
+    job: 'web developer',
+    image:
+      '/images/person-1.jpg',
+    text:
+      "Consistency is the key and he did an excellent work ",
+  },
+  {
+    id: 2,
+    name: 'Anna Johnson',
+    job: 'web designer',
+    image:
+      '/images/person-2.jpg',
+    text:
+      'He is hardworking and a peoples person and can work effectively under pressure.',
+  },
+  {
+    id: 3,
+    name: 'Fernando Carracosa',
+    job: 'Senior Front End Developer',
+    image:
+      '/images/fernando.jpg',
+    text:
+      'He started out a cocoon but he has now transform into a butterfly.So happy to see him where he is right now.',
+  },
+  {
+    id: 4,
+    name: 'Arjen Broek',
+    job: 'the boss',
+    image:
+      '/images/arjen.jpg',
+    text:
+      'Our first conversion was memorable.It was fun working with you.All the best for your future endevours',
+  },
+];
+
+export default reviews;
